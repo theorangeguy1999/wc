eaa12eb9c3f73f27da04d0f653361ce35403156e
Sankalp <sankalp.sharma99@gmail.com>
2022-03-07 20:33:25 +0530

    Added BadgerDB to store webpages

--- go.mod
+++ go.mod
@@ -8,15 +8,28 @@ require (
 	github.com/antchfx/htmlquery v1.2.4 // indirect
 	github.com/antchfx/xmlquery v1.3.9 // indirect
 	github.com/antchfx/xpath v1.2.0 // indirect
+	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/dgraph-io/badger/v3 v3.2103.2 // indirect
+	github.com/dgraph-io/ristretto v0.1.0 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gocolly/colly v1.2.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/google/flatbuffers v1.12.1 // indirect
 	github.com/jawher/mow.cli v1.2.0 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/klauspost/compress v1.12.3 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
+	go.opencensus.io v0.22.5 // indirect
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
--- web_crawler.go
+++ web_crawler.go
@@ -5,6 +5,8 @@ import (
 	"time" // to measure time
 	"github.com/gocolly/colly" // web crawler package for go
 	"github.com/gocolly/colly/queue"
+	"github.com/gocolly/colly/extensions"
+	badger "github.com/dgraph-io/badger/v3" //badgerdb API
 )
 
 func main(){
@@ -12,42 +14,75 @@ func main(){
 	start := time.Now() //Start time
 	link_count := 0 // Counter for links visited
 	count_checker := 1 // Checker for 1 , 10 ,100 .... pages
+	crawling_limit := 1000000 //To set total number of pages crawled
+
+	//opening database
+	opt := badger.DefaultOptions("\\dbbadger")
+	db,err := badger.Open(opt)
+
+	if err!=nil{
+		fmt.Println(err)
+		return
+	}
+
+	//starting a transcation
+	txn := db.NewTransaction(true)
 
 	// Initializing Colly Collector Object
 	c := colly.NewCollector(
 		colly.AllowedDomains("www.en.wikipedia.org","en.wikipedia.org"), //Only english wikipedia pages will be crawled through
 	)
 
+	extensions.RandomUserAgent(c)
+
 	// Creating a queue for links to go through
 	q, _ := queue.New(
-		16, // Number of consumer threads
-		&queue.InMemoryQueueStorage{MaxSize: 100000}, // Queue size
+		20, // Number of consumer threads
+		&queue.InMemoryQueueStorage{MaxSize: 10000}, // Queue size
 	)
 
 	// Parsing this element of HTML (content) for links
 	c.OnHTML(".mw-parser-output p a[href]", func(e *colly.HTMLElement) {
 
-		link := e.Attr("href")
-		if link_count >= 1000001{
+		link := e.Request.AbsoluteURL(e.Attr("href"))
+		queue_size, err := q.Size()
+		if err != nil{
+			fmt.Println(err)
+			return
+		}
+		if link_count >= crawling_limit || queue_size >= 1000{
 			return;
 		} else {
-			q.AddURL(e.Request.AbsoluteURL(link)) //Add link to queue
+			q.AddURL(link) //Add link to queue
 		}
 	})
 
 	c.OnResponse(func(r *colly.Response) {
 
 		link_count = link_count + 1 // a new link is visited
-
 		//Print time stats
 		if link_count == count_checker{
 			fmt.Println("Crawled through ",count_checker," page(s) in time: ",time.Now().Sub(start))
 			count_checker = count_checker * 10
 		}
+
+		//add to transaction
+		err := txn.Set([]byte(r.Request.URL.RawPath),r.Body)
+
+		//if transaction size becomes too big then commit and start new transaction
+		if err == badger.ErrTxnTooBig{
+			_ = txn.Commit()
+			// fmt.Println("commited to database at link count: ", link_count)
+			txn = db.NewTransaction(true)
+			_ = txn.Set([]byte(r.Request.URL.RawPath),r.Body)
+		}
 	})
 
 	q.AddURL("https://en.wikipedia.org/wiki/Web_crawler") //Seed Url
+	start = time.Now()
 	q.Run(c) //Start Crawling
+	txn.Commit() //Commit to database
 
+	defer db.Close() //Close database
 	return
 }
